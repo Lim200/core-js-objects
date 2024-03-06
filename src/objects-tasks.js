d5743911f083a1b2bec2d54192eb10e2a2d91d37
Lim200 <gsv475@gmail.com>
2024-03-06 15:41:21 +0300

    fix: fiw 1 warning

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -227,7 +227,7 @@ function Rectangle(width, height) {
   this.width = width;
   this.height = height;
 
-  this.getArea = function () {
+  this.getArea = () => {
     return this.width * this.height;
   };
 }
